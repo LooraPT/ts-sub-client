090a6f5ee104f732f61917f870fb09795a3a10f8
Volodymyr Romanenko <vova0220romanenko@gmail.com>
2023-03-22 21:08:43 +0200

    mockdata correction

--- src/store/reducers/subscriptions/mockDataSubscription.ts
+++ src/store/reducers/subscriptions/mockDataSubscription.ts
@@ -5,28 +5,28 @@ export const subscriptionMock: ISubscription[] = [
         id: '1',
         name: "Netflix",
         img: '',
-        description: 'fdfdffffffffffffffffffffffffffffffffdfdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         levels: []
     },
     {
         id: '2',
         name: "YouTube Premium",
         img: '',
-        description: 'fdfdffffffffffffffffffffffffffdfdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         levels: []
     },
     {
         id: '3',
         name: "Spotify",
         img: '',
-        description: 'fdfdfffffffffffffffffffffffffffffffffdfdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         levels: []
     },
     {
         id: '4',
         name: "Spotify",
         img: '',
-        description: 'fdfdffffffffffffffffffffffffffffffffdfdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         levels: []
     },
 ]
